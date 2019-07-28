eb503628f446e4be08bb3504a964759150383721
Mohamed El Sherif <mohamed.elsherif@thoughtworks.com>
2019-07-28 15:19:03 +0200

    Lower case

--- php/navigation.php
+++ php/navigation.php
@@ -2,13 +2,13 @@
 
     <!-- Nav tabs -->
     <ul class="nav nav-tabs removeBorderBottom" role="tablist">
-        <li class="nav_tab_li" id="theNav0"><a id= "ABOUT" href="#" onClick="goto(0, this); return false">ABOUT</a></li>
-        <li class="nav_tab_li" id="theNav1"><a id= "ARTWORK" href="#" onClick="goto(1, this); return false">ARTWORK</a></li>
-        <li class="nav_tab_li" id="theNav2"><a id= "EXHIBITIONS" href="#" onClick="goto(2, this); return false">EXHIBITIONS</a></li>
-        <li class="nav_tab_li" id="theNav3"><a id= "PRINTS" href="#" onClick="goto(3, this); return false">PRINTS</a></li>
-        <li class="nav_tab_li" id="theNav4"><a id= "COURSES" href="#" onClick="goto(4, this); return false">COURSES</a></li>
-        <li class="nav_tab_li" id="theNav5"><a id= "ARTICLES" href="#" onClick="goto(5, this); return false">ARTICLES</a></li>
-        <li class="nav_tab_li" id="theNav6"><a id= "CONTACT" href="#" onClick="goto(6, this); return false">CONTACT</a></li>
+        <li class="nav_tab_li" id="theNav0"><a id= "about" href="#" onClick="goto(0, this); return false">ABOUT</a></li>
+        <li class="nav_tab_li" id="theNav1"><a id= "artwork" href="#" onClick="goto(1, this); return false">ARTWORK</a></li>
+        <li class="nav_tab_li" id="theNav2"><a id= "exhibitions" href="#" onClick="goto(2, this); return false">EXHIBITIONS</a></li>
+        <li class="nav_tab_li" id="theNav3"><a id= "prints" href="#" onClick="goto(3, this); return false">PRINTS</a></li>
+        <li class="nav_tab_li" id="theNav4"><a id= "courses" href="#" onClick="goto(4, this); return false">COURSES</a></li>
+        <li class="nav_tab_li" id="theNav5"><a id= "articles" href="#" onClick="goto(5, this); return false">ARTICLES</a></li>
+        <li class="nav_tab_li" id="theNav6"><a id= "contact" href="#" onClick="goto(6, this); return false">CONTACT</a></li>
 
     </ul>
 </div>
